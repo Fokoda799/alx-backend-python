c1b5a18a0a6e679a184f7a3b03f4fe2ae2619491
feyt2003 <feyt2003@gmail.com>
2024-07-23 20:20:36 +0000

    ok

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Sum List """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Sum list """
     sum: float = 0
     for num in input_list:
