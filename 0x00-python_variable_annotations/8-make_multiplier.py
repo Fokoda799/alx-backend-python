3b537d7d45dba19f037d1e0cec51442e55bf31f0
feyt2003 <feyt2003@gmail.com>
2024-07-23 20:46:34 +0000

    fix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Complex types - functions """
-from typing import callable
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ Returns a function that multiplies a float by multiplier """
     return lambda x: x * multiplier
