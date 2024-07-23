3de820b71c5bc1bd53fb3bd1f1844c17b757db86
feyt2003 <feyt2003@gmail.com>
2024-07-23 20:34:30 +0000

    ok

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Complex types - tuple """
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Returns a tuple """
+    return (k, v ** 2)
