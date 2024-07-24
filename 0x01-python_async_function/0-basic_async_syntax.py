4d2453fa80dd718c887e7618aa9fd146d664e63e
feyt2003 <feyt2003@gmail.com>
2024-07-24 20:28:33 +0000

    pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ from random import randrange
 import asyncio
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ Wait random time """
     delaiy: float = randrange(0, max_delay + 1) / 10
-    await asyncio.sleep(delaiy) 
-    return delaiy 
+    await asyncio.sleep(delaiy)
+    return delaiy
