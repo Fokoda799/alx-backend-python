01fa9e72adea9a5d79b0d33fc668bd06f2ab438f
feyt2003 <feyt2003@gmail.com>
2024-07-24 20:26:19 +0000

    fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ The basics of async """
-from typing import Union
 from random import randrange
 import asyncio
 
 
-async def wait_random(max_delay: int=10) -> Union[int, float]:
+async def wait_random(max_delay: int=10) -> float:
     """ Wait random time """
-    delaiy: Union[int, float] = randrange(0, max_delay + 1) / 10
+    delaiy: float = randrange(0, max_delay + 1) / 10
     await asyncio.sleep(delaiy) 
     return delaiy 
